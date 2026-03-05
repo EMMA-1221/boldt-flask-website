8beccd00653f97988162d2e7f6ae892f3f331805
EMMA-1221 <lzhang772@wisc.edu>
2026-03-05 15:15:24 -0600

    755 line code in static/app.js

--- Website Hosting/static/app.js
+++ Website Hosting/static/app.js
@@ -172,7 +172,7 @@ r_e("submit_form").addEventListener("click", () => {
     r_e("errors_list").innerHTML += "Fix errors above to run calculator <br>";
   } else {
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://127.0.0.1:5000/", true);
+    xhr.open("POST", "https://boldt-flask-website.onrender.com/", true);
     xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
     let body = JSON.stringify({
       industry: r_e("Industry").value,
@@ -224,3 +224,4 @@ r_e("submit_form").addEventListener("click", () => {
     };
   }
 });
+

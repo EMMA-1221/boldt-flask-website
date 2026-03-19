ee113b9f56f01a373923caeb3b5993f6507cd0cc
EMMA-1221 <lzhang772@wisc.edu>
2026-03-19 15:25:29 -0500

    Update Flask app to use Railway host and port

--- Website Hosting/website.py
+++ Website Hosting/website.py
@@ -58,4 +58,6 @@ def home():
         return json.dumps(response)        
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port = 5000, debug=True, threaded=False) # don't change this line!
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)

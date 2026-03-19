3cdf3753ea2cb987de65d53d20f76a7aef734478
EMMA-1221 <lzhang772@wisc.edu>
2026-03-19 15:26:18 -0500

    delete"import os'

--- Website Hosting/website.py
+++ Website Hosting/website.py
@@ -58,6 +58,6 @@ def home():
         return json.dumps(response)        
 
 if __name__ == '__main__':
-    import os
+    
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)

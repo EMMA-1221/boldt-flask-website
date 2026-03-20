6f8b8eb1632bda2f392f427e0aa60779a8c1bef2
EMMA-1221 <lzhang772@wisc.edu>
2026-03-19 20:37:16 -0500

    change in comments

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ function hide() {
   r_e("home_div").classList.add("is-hidden");
   r_e("guidance_div").classList.add("is-hidden");
   r_e("estimator_div").classList.add("is-hidden");
-  //   r_e("guidance_dropdown").classList.remove("is-active");
 }
 
 // home button
@@ -76,7 +75,9 @@ r_e("clear_form").addEventListener("click", () => {
   r_e("errors_list").innerHTML = "";
 });
 
-let team_names = ["member1", "member2", "member3", "member4"];
+// ✅ CHANGE 1: Removed hardcoded team_names declaration.
+// team_names is now injected via <script> in index.html from Flask.
+// let team_names = ["member1", "member2", "member3", "member4"];  <-- deleted
 team_names.forEach((member) => {
   r_e("members").innerHTML += `<option value="${member}"></option>`;
 });
@@ -106,89 +107,49 @@ r_e("add_team_mem").addEventListener("click", () => {
 
 // post information
 r_e("submit_form").addEventListener("click", () => {
-  // test for errors
-  // clear errors list
   r_e("errors_list").innerHTML = "";
-  // test industry for selection
   if (r_e("Industry").value == "empty") {
     r_e("errors_list").innerHTML += "Missing industry, please select one. <br>";
   }
-  // test revenue for selection
   if (r_e("Revenue").value == "") {
-    r_e("errors_list").innerHTML +=
-      "Missing project revenue, please enter one. <br>";
-  }
-  // test revenue for invalid number
-  else if (isNaN(Number(r_e("Revenue").value.replaceAll(",", "")))) {
-    r_e("errors_list").innerHTML +=
-      "Invalid Project Revenue, enter as described above and try again <br>";
+    r_e("errors_list").innerHTML += "Missing project revenue, please enter one. <br>";
+  } else if (isNaN(Number(r_e("Revenue").value.replaceAll(",", "")))) {
+    r_e("errors_list").innerHTML += "Invalid Project Revenue, enter as described above and try again <br>";
   } else if (Number(r_e("Revenue").value.replaceAll(",", "")) <= 0) {
-    r_e("errors_list").innerHTML +=
-      "Invalid Project Revenue, must be greater than 0 <br>";
+    r_e("errors_list").innerHTML += "Invalid Project Revenue, must be greater than 0 <br>";
   }
-  // test length for selection
   if (r_e("Length").value == "") {
-    r_e("errors_list").innerHTML +=
-      "Missing project length, please enter one. <br>";
-  }
-  // test length for invalid number
-  else if (isNaN(Number(r_e("Length").value.replaceAll(",", "")))) {
-    r_e("errors_list").innerHTML +=
-      "Invalid Project Length, enter as described above and try again <br>";
+    r_e("errors_list").innerHTML += "Missing project length, please enter one. <br>";
+  } else if (isNaN(Number(r_e("Length").value.replaceAll(",", "")))) {
+    r_e("errors_list").innerHTML += "Invalid Project Length, enter as described above and try again <br>";
   } else if (Number(r_e("Length").value.replaceAll(",", "")) <= 0) {
-    r_e("errors_list").innerHTML +=
-      "Invalid Project Length, must be greater than 0 <br>";
+    r_e("errors_list").innerHTML += "Invalid Project Length, must be greater than 0 <br>";
   }
-  // test contract type for selection
   if (r_e("Contract").value == "empty") {
-    r_e("errors_list").innerHTML +=
-      "Missing contract type, please select one. <br>";
+    r_e("errors_list").innerHTML += "Missing contract type, please select one. <br>";
   }
-  // test zip for selection
   if (r_e("Zip").value == "") {
-    r_e("errors_list").innerHTML +=
-      "Missing project Zip code, please enter one. <br>";
-  }
-  // test zip for invalid number
-  else if (isNaN(Number(r_e("Zip").value))) {
-    r_e("errors_list").innerHTML +=
-      "Invalid Zip Code, enter 5 digits only and try again. <br>";
+    r_e("errors_list").innerHTML += "Missing project Zip code, please enter one. <br>";
+  } else if (isNaN(Number(r_e("Zip").value))) {
+    r_e("errors_list").innerHTML += "Invalid Zip Code, enter 5 digits only and try again. <br>";
   } else if (r_e("Zip").value.length != 5) {
-    r_e("errors_list").innerHTML +=
-      "Invalid Zip Code, enter 5 digits only and try again. <br>";
+    r_e("errors_list").innerHTML += "Invalid Zip Code, enter 5 digits only and try again. <br>";
   }
-  // test team members
   for (let x = 1; x <= num_members; x++) {
-    if (
-      !team_names.includes(r_e(`team_mem_${x}`).value) &&
-      r_e(`team_mem_${x}`).value != ""
-    ) {
-      r_e(
-        "errors_list"
-      ).innerHTML += `Team Member ${x} name not found. Check name again or contract administrator to get them added to the database. <br>`;
-    } else if (
-      r_e(`team_mem_${x}_role`).value == "" &&
-      r_e(`team_mem_${x}`).value != ""
-    ) {
-      r_e(
-        "errors_list"
-      ).innerHTML += `Missing Team Member ${x}'s role. Enter role above. <br>`;
-    } else if (
-      !roles.includes(r_e(`team_mem_${x}_role`).value) &&
-      r_e(`team_mem_${x}_role`).value != ""
-    ) {
-      r_e(
-        "errors_list"
-      ).innerHTML += `Team Member ${x}'s role not available. Select from one of the 5 roles above. <br>`;
+    if (!team_names.includes(r_e(`team_mem_${x}`).value) && r_e(`team_mem_${x}`).value != "") {
+      r_e("errors_list").innerHTML += `Team Member ${x} name not found. Check name again or contract administrator to get them added to the database. <br>`;
+    } else if (r_e(`team_mem_${x}_role`).value == "" && r_e(`team_mem_${x}`).value != "") {
+      r_e("errors_list").innerHTML += `Missing Team Member ${x}'s role. Enter role above. <br>`;
+    } else if (!roles.includes(r_e(`team_mem_${x}_role`).value) && r_e(`team_mem_${x}_role`).value != "") {
+      r_e("errors_list").innerHTML += `Team Member ${x}'s role not available. Select from one of the 5 roles above. <br>`;
     }
   }
 
-  // run calculation
   if (r_e("errors_list").innerHTML != "") {
     r_e("errors_list").innerHTML += "Fix errors above to run calculator <br>";
   } else {
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://boldt-flask-website.onrender.com/", true);
+    xhr.open("POST", "/", true);  // 改动2：从 "http://127.0.0.1:5000/" 改为 "/" (相对路径)
     xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
     let body = JSON.stringify({
       industry: r_e("Industry").value,
@@ -217,14 +178,6 @@ r_e("submit_form").addEventListener("click", () => {
       name_10: r_e("team_mem_10").value,
       role_10: r_e("team_mem_10_role").value,
     });
-    xhr.onload = () => {
-      if (xhr.readyState == 4 && xhr.status == 201) {
-        console.log(JSON.parse(xhr.responseText));
-      } else {
-        console.log(`Error: ${xhr.status}`);
-      }
-    };
-    xhr.send(body);
     xhr.onload = function () {
       answers = JSON.parse(xhr.response);
       if (Object.keys(answers).length == 1) {
@@ -238,6 +191,6 @@ r_e("submit_form").addEventListener("click", () => {
       r_e("average_val").innerHTML = answers.Average;
       r_e("result_val").innerHTML = answers.Result;
     };
+    xhr.send(body);
   }
 });
-

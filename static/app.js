da4950f9c2336477524f0c289d3b3dc20a1dbff0
EMMA-1221 <lzhang772@wisc.edu>
2026-04-23 15:33:15 -0500

    Refactor event listeners and form handling

--- static/app.js
+++ static/app.js
@@ -8,7 +8,6 @@ function hide() {
   r_e("home_div").classList.add("is-hidden");
   r_e("guidance_div").classList.add("is-hidden");
   r_e("estimator_div").classList.add("is-hidden");
-  //   r_e("guidance_dropdown").classList.remove("is-active");
 }
 
 // home button
@@ -29,9 +28,25 @@ r_e("estimator_button").addEventListener("click", () => {
   r_e("estimator_div").classList.remove("is-hidden");
 });
 
-// guidance dropdown button
+// guidance dropdown
 r_e("guidance_filter").addEventListener("change", () => {
-  console.log("changed!");
+  // NEEDS TO BE CODED
+});
+
+// tool button
+r_e("tool_button").addEventListener("click", () => {
+  r_e("tool_hints").classList.add("is-hidden");
+  r_e("estimator_tool").classList.remove("is-hidden");
+  r_e("tool_button").classList.add("is-active");
+  r_e("hints_button").classList.remove("is-active");
+});
+
+// hints button
+r_e("hints_button").addEventListener("click", () => {
+  r_e("estimator_tool").classList.add("is-hidden");
+  r_e("tool_hints").classList.remove("is-hidden");
+  r_e("tool_button").classList.remove("is-active");
+  r_e("hints_button").classList.add("is-active");
 });
 
 // clear form
@@ -45,27 +60,20 @@ r_e("clear_form").addEventListener("click", () => {
     r_e(`team_mem_${x}`).value = "";
     r_e(`team_mem_${x}_role`).value = "";
   }
-  let tbl_val = [
-    "overall_val",
-    "industry_val",
-    "revenue_val",
-    "length_val",
-    "contract_val",
-    "average_val",
-    "result_val",
-  ];
-  tbl_val.forEach((cell) => {
-    r_e(`${cell}`).innerHTML = "";
-  });
+  r_e("results_body").innerHTML = "";
+  r_e("average_cim_val").innerHTML = "";
+  r_e("average_fpm_val").innerHTML = "";
+  r_e("result_val").innerHTML = "";
   r_e("errors_list").innerHTML = "";
 });
 
+// team_names injected from Flask
 team_names.forEach((member) => {
   r_e("members").innerHTML += `<option value="${member}"></option>`;
 });
 let roles = ["PM", "PE", "PEx", "Sup", "PC"];
 
-// add team members
+// add/remove team members
 let num_members = 2;
 r_e("rm_team_mem").addEventListener("click", () => {
   if (num_members > 1) {
@@ -87,86 +95,47 @@ r_e("add_team_mem").addEventListener("click", () => {
   }
 });
 
-// post information
+// submit form
 r_e("submit_form").addEventListener("click", () => {
-  // test for errors
-  // clear errors list
   r_e("errors_list").innerHTML = "";
-  // test industry for selection
+
   if (r_e("Industry").value == "empty") {
     r_e("errors_list").innerHTML += "Missing industry, please select one. <br>";
   }
-  // test revenue for selection
   if (r_e("Revenue").value == "") {
-    r_e("errors_list").innerHTML +=
-      "Missing project revenue, please enter one. <br>";
-  }
-  // test revenue for invalid number
-  else if (isNaN(Number(r_e("Revenue").value.replaceAll(",", "")))) {
-    r_e("errors_list").innerHTML +=
-      "Invalid Project Revenue, enter as described above and try again <br>";
+    r_e("errors_list").innerHTML += "Missing project revenue, please enter one. <br>";
+  } else if (isNaN(Number(r_e("Revenue").value.replaceAll(",", "")))) {
+    r_e("errors_list").innerHTML += "Invalid Project Revenue, enter as described above and try again <br>";
   } else if (Number(r_e("Revenue").value.replaceAll(",", "")) <= 0) {
-    r_e("errors_list").innerHTML +=
-      "Invalid Project Revenue, must be greater than 0 <br>";
+    r_e("errors_list").innerHTML += "Invalid Project Revenue, must be greater than 0 <br>";
   }
-  // test length for selection
   if (r_e("Length").value == "") {
-    r_e("errors_list").innerHTML +=
-      "Missing project length, please enter one. <br>";
-  }
-  // test length for invalid number
-  else if (isNaN(Number(r_e("Length").value.replaceAll(",", "")))) {
-    r_e("errors_list").innerHTML +=
-      "Invalid Project Length, enter as described above and try again <br>";
+    r_e("errors_list").innerHTML += "Missing project length, please enter one. <br>";
+  } else if (isNaN(Number(r_e("Length").value.replaceAll(",", "")))) {
+    r_e("errors_list").innerHTML += "Invalid Project Length, enter as described above and try again <br>";
   } else if (Number(r_e("Length").value.replaceAll(",", "")) <= 0) {
-    r_e("errors_list").innerHTML +=
-      "Invalid Project Length, must be greater than 0 <br>";
+    r_e("errors_list").innerHTML += "Invalid Project Length, must be greater than 0 <br>";
   }
-  // test contract type for selection
   if (r_e("Contract").value == "empty") {
-    r_e("errors_list").innerHTML +=
-      "Missing contract type, please select one. <br>";
+    r_e("errors_list").innerHTML += "Missing contract type, please select one. <br>";
   }
-  // test zip for selection
   if (r_e("Zip").value == "") {
-    r_e("errors_list").innerHTML +=
-      "Missing project Zip code, please enter one. <br>";
-  }
-  // test zip for invalid number
-  else if (isNaN(Number(r_e("Zip").value))) {
-    r_e("errors_list").innerHTML +=
-      "Invalid Zip Code, enter 5 digits only and try again. <br>";
+    r_e("errors_list").innerHTML += "Missing project Zip code, please enter one. <br>";
+  } else if (isNaN(Number(r_e("Zip").value))) {
+    r_e("errors_list").innerHTML += "Invalid Zip Code, enter 5 digits only and try again. <br>";
   } else if (r_e("Zip").value.length != 5) {
-    r_e("errors_list").innerHTML +=
-      "Invalid Zip Code, enter 5 digits only and try again. <br>";
+    r_e("errors_list").innerHTML += "Invalid Zip Code, enter 5 digits only and try again. <br>";
   }
-  // test team members
   for (let x = 1; x <= num_members; x++) {
-    if (
-      !team_names.includes(r_e(`team_mem_${x}`).value) &&
-      r_e(`team_mem_${x}`).value != ""
-    ) {
-      r_e(
-        "errors_list"
-      ).innerHTML += `Team Member ${x} name not found. Check name again or contract administrator to get them added to the database. <br>`;
-    } else if (
-      r_e(`team_mem_${x}_role`).value == "" &&
-      r_e(`team_mem_${x}`).value != ""
-    ) {
-      r_e(
-        "errors_list"
-      ).innerHTML += `Missing Team Member ${x}'s role. Enter role above. <br>`;
-    } else if (
-      !roles.includes(r_e(`team_mem_${x}_role`).value) &&
-      r_e(`team_mem_${x}_role`).value != ""
-    ) {
-      r_e(
-        "errors_list"
-      ).innerHTML += `Team Member ${x}'s role not available. Select from one of the 5 roles above. <br>`;
+    if (!team_names.includes(r_e(`team_mem_${x}`).value) && r_e(`team_mem_${x}`).value != "") {
+      r_e("errors_list").innerHTML += `Team Member ${x} name not found. Check name again or contact administrator to get them added to the database. <br>`;
+    } else if (r_e(`team_mem_${x}_role`).value == "" && r_e(`team_mem_${x}`).value != "") {
+      r_e("errors_list").innerHTML += `Missing Team Member ${x}'s role. Enter role above. <br>`;
+    } else if (!roles.includes(r_e(`team_mem_${x}_role`).value) && r_e(`team_mem_${x}_role`).value != "") {
+      r_e("errors_list").innerHTML += `Team Member ${x}'s role not available. Select from one of the 5 roles above. <br>`;
     }
   }
 
-  // run calculation
   if (r_e("errors_list").innerHTML != "") {
     r_e("errors_list").innerHTML += "Fix errors above to run calculator <br>";
   } else {
@@ -200,27 +169,32 @@ r_e("submit_form").addEventListener("click", () => {
       name_10: r_e("team_mem_10").value,
       role_10: r_e("team_mem_10_role").value,
     });
-    xhr.onload = () => {
-      if (xhr.readyState == 4 && xhr.status == 201) {
-        console.log(JSON.parse(xhr.responseText));
-      } else {
-        console.log(`Error: ${xhr.status}`);
-      }
-    };
-    xhr.send(body);
+
     xhr.onload = function () {
       answers = JSON.parse(xhr.response);
-      if (Object.keys(answers).length == 1) {
+
+      if (answers.error) {
         r_e("errors_list").innerHTML += answers.error;
+        return;
       }
-      r_e("overall_val").innerHTML = answers.Overall;
-      r_e("industry_val").innerHTML = answers.Industry;
-      r_e("revenue_val").innerHTML = answers.Revenue;
-      r_e("length_val").innerHTML = answers.Length;
-      r_e("contract_val").innerHTML = answers.Contract;
-      r_e("average_val").innerHTML = answers.Average;
-      r_e("result_val").innerHTML = answers.Result;
+
+      // populate table - one row per team member
+      r_e("results_body").innerHTML = "";
+      answers.members.forEach((member) => {
+        let row = `<tr>
+          <td>${member.name} (${member.role})</td>
+          <td>${member.cim}</td>
+          <td>${member.fpm}</td>
+        </tr>`;
+        r_e("results_body").innerHTML += row;
+      });
+
+      // average and result
+      r_e("average_cim_val").innerHTML = answers.avg_cim;
+      r_e("average_fpm_val").innerHTML = answers.avg_fpm;
+      r_e("result_val").innerHTML = answers.result;
     };
+
+    xhr.send(body);
   }
 });
-
